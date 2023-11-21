13bb06d8682124541a91363362f911066829d80c
RicardoEgas <rj_egas@hotmail.com>
2023-11-21 16:51:03 +0100

    Update application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'foods#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'recipes#index'
   resources :foods, only: [:index, :new, :create, :destroy]
   resources :recipes, only: %i[index show new create]
 end
