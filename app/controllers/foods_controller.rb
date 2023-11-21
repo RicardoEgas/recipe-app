baaafd8afa70d3a3c20e8141a2205850a9b09107
redwan <redtah2007@yahoo.com>
2023-11-21 14:55:32 +0300

    Add foods_list model and controller

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,29 @@
+class FoodsController < ApplicationController
+  def index
+    @foods = Food.all.order(:name)
+  end
+
+  def show
+    set_food
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user = current_user
+    if @food.save
+      redirect_to @food, notice: 'Food was successfully created.'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
+
+  def set_food
+    @food = Food.find(params[:id])
+  end
+end
