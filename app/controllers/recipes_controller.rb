9023e054ae5c98682d915d341b04551f5b7809d3
RicardoEgas <rj_egas@hotmail.com>
2023-11-21 14:38:35 +0100

    Setup model, controller and views for recipes

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,29 @@
+class RecipesController < ApplicationController
+    def index
+      @recipes = Recipe.all
+    end
+
+    def new
+        @recipe = Recipe.new
+      end
+  
+    def show
+      @recipe = Recipe.find(params[:id])
+    end
+
+    def create
+        @recipe = Recipe.new(recipe_params)
+    
+        if @recipe.save
+          redirect_to recipes_path, notice: 'Recipe was successfully created.'
+        else
+          render :new
+        end
+      end
+
+      private
+
+      def recipe_params
+        params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description)
+      end
+  end
