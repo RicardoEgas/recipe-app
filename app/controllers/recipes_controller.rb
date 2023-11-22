1eea647a6352d915cecabd769bbbd383be2b3de6
redwan <redtah2007@yahoo.com>
2023-11-22 15:25:38 +0300

    Add recipes_foods_controller and shope_list view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  
+  before_action :authenticate_user!, only: %i[new create]
+
   def index
     @recipes = current_user.recipes.all
   end
@@ -14,8 +14,18 @@ class RecipesController < ApplicationController
     @recipe_foods = @recipe.recipe_foods.includes(:food)
   end
 
+  # def create
+  #   @recipe = Recipe.new(recipe_params)
+
+  #   if @recipe.save
+  #     redirect_to recipes_path, notice: 'Recipe was successfully created.'
+  #   else
+  #     render :new
+  #   end
+  # end
+
   def create
-    @recipe = Recipe.new(recipe_params)
+    @recipe = current_user.recipes.build(recipe_params)
 
     if @recipe.save
       redirect_to recipes_path, notice: 'Recipe was successfully created.'
@@ -24,9 +34,28 @@ class RecipesController < ApplicationController
     end
   end
 
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path, notice: 'Recipe was successfully removed.'
+  end
+
+  def shopping_list
+    @recipe = Recipe.includes([recipe_foods: [:food]]).find(params[:recipe_id])
+    @recipe_data = {
+      total_food_items: @recipe.recipe_foods.size,
+      total_price: @recipe.recipe_foods.sum { |recipe_food| recipe_food.food.price * recipe_food.quantity }
+    }
+    # @recipe_foods = @recipe.recipe_foods.includes(:food)
+  end
+
   private
 
+  # def recipe_params
+  #   params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  # end
+
   def recipe_params
-    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  get '/recipes/:recipe_id/shopping_list', to: 'recipes#shopping_list', as: 'shopping_list'
   devise_scope :user do
     authenticated :user do
-      root :to => 'foods#index', as: :authenticated_root
+      root to: 'foods#index', as: :authenticated_root
     end
     unauthenticated :user do
-      root :to => 'devise/sessions#new', as: :unauthenticated_root
+      root to: 'devise/sessions#new', as: :unauthenticated_root
     end
   end
 
   resources :users, only: [] do
-    resources :foods, only: [:index, :show, :new, :create, :destroy]
-    resources :recipes, only: [:index, :new, :create, :show, :destroy]
+    resources :foods, only: %i[index show new create destroy]
+    resources :recipes, only: %i[index new create show destroy]
   end
 
-  resources :recipes, only: [:index, :new, :create, :show, :update, :destroy] do
+  resources :recipes, only: %i[index new create show update destroy] do
     resources :recipe_foods
   end
 
-  resources :foods, only: [:index, :new, :create, :destroy]
+  resources :foods, only: %i[index new create destroy]
 end
